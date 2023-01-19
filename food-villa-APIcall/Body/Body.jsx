c83b0de3921cca0bedb2ee50b2adb1dcc52a57f1
Sheth <astha.sheth@capgemini.com>
2023-01-19 12:55:01 +0530

    apiCall

---
+++ food-villa-APIcall/Body/Body.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+
+const RestaurantCard = ({ name, cuisines, cloudinaryImageId, avgRating }) => (
+  <div className="card">
+    <img
+      src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`}
+      alt="Jagdish"
+    />
+    <h2>{name}</h2>
+    <h3>{cuisines?.join(", ")}</h3>
+
+    <h4>{avgRating} stars</h4>
+  </div>
+);
+const Body = () => {
+  useEffect(() => {
+    (async () => {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6105073&lng=77.1145653&page_type=DESKTOP_WEB_LISTING"
+      );
+      console.log(data, "DATA");
+    })();
+  });
+  return (
+    <div className="restaurant-list">
+      {/* {data.map((restaurant) => (
+        <RestaurantCard {...restaurant.data} />
+      ))} */}
+    </div>
+  );
+};
+
+export default Body;
---
+++ food-villa-MUltiObjArray/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../food-villa-hardcoded-singleObjArray/Footer/Footer";
+import Header from "../food-villa-hardcoded-singleObjArray/Header/Header";
+import Body from "./Body/Body";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
